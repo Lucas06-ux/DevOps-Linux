308c26840ccee122bce3624b95793ab6799d56be
Ubuntu <azureuser@DevOps564508.fpyes3wsho0udmazbj3u2dkeqg.jnbx.internal.cloudapp.net>
2026-04-28 23:37:35 +0000

    Projeto Spring Boot com MySQL na Azure

---
+++ src/main/java/com/projeto/controller/UsuarioController.java
@@ -0,0 +1,28 @@
+package com.projeto.controller;
+
+import com.projeto.model.Usuario;
+import com.projeto.repository.UsuarioRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioRepository repository;
+
+    public UsuarioController(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Usuario> listar() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Usuario criar(@RequestBody Usuario usuario) {
+        return repository.save(usuario);
+    }
+}
